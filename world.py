c92817092be03eba8d5eec2922a42a9950c88181
Nathaanlennon <nathaanbias@gmail.com>
2025-10-20 18:28:58 +0200

    collision pour les event (/entités?)

--- world.py
+++ world.py
@@ -6,7 +6,6 @@ class Test(World):
     def __init__(self, data, **kwargs):
         super().__init__(data,"assets/maps/maptest.txt", (2, 3))
         self.name = "Monde1"
-        self.map_data = self.load_map()
 
         self.event_system.add_event(MoveEvent(data, self,"door", (4,11), Test2, (5,5),"door"))
         self.event_system.add_event(MoveEvent(data, self,"teleporter", (5,1), Test, (3,3),"teleport"))
@@ -16,11 +15,13 @@ class Test2(World):
     def __init__(self, data, **kwargs):
         super().__init__(data,"assets/maps/testchateau.txt", (5, 5))
         self.name = "Monde2"
-        self.map_data = self.load_map()
 
+        self.event_system.add_event(MoveEvent(data, self, "door", (8, 24), Test3, (2, 1), "door"))
 
 class Test3(World):
     def __init__(self, data, **kwargs):
         super().__init__(data,"assets/maps/test_village.txt", (3, 3))
         self.name = "Monde3"
-        self.map_data = self.load_map()
+
+        self.event_system.add_event(MoveEvent(data, self, "door", (1, 1), Test2, (9, 24), "teleport"))
+
