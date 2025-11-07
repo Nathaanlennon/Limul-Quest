1e094313256a11eb4c964bd2be8cce601e8c135a
eliot1412 <eliotddg@student-laptop.localdomain>
2025-11-07 19:09:31 +0100

    temp

--- engine/core/base.py
+++ engine/core/base.py
@@ -15,7 +15,7 @@ else:
 
 class UniverseData:
     def __init__(self, scene, **kwargs):
-        self.size = (37,71)
+        self.size = (39,72)
         self.scenes = {}
         self.current_scene = None
         self.player = Player(self.current_scene, self.current_scene.spawn_player if self.current_scene else (2, 2))
--- world.py
+++ world.py
@@ -3,20 +3,47 @@ from engine.core.base import World, Entity, Event, NPC
 
 class Test(World):
     def __init__(self, data, **kwargs):
-        super().__init__(data, "assets/maps/maptest.txt", (2, 3))
+        super().__init__(data, "assets/maps/village2.txt", (12, 30))
         self.name = "Monde1"
 
-        self.add_entity(Entity(self, "door1", (4, 11), 'D',
+        #bucher's door
+
+        self.add_entity(Entity(self, "door1", (9, 13), 'D',
                                [Event(data, self,"door1", "ON_INTERACT", "MOVE",
                   target_scene=Test2, target_position=(5, 5))]))
-        self.add_entity(Entity(self, "teleporter1", (5, 1), 'T',
-                               [Event(data, self,"teleporter1", "ON_STEP", "MOVE",
-                  target_scene=Test, target_position=(3, 3))]))
-        self.add_entity(Entity(self, "teleporter1", (5, 1), 'T',
-                               [Event(data, self,"teleporter1", "ON_STEP", "MOVE",
-                  target_scene=Test, target_position=(3, 3))]))
+        self.add_entity(Entity(self, "door2", (9, 14), 'D',
+                               [Event(data, self,"door2", "ON_INTERACT", "MOVE",
+                  target_scene=Test2, target_position=(5, 5))]))
+        
+        #theatre door
 
-        self.add_entity(NPC(self, "npc1", (1, 1), 'N', dialogue="assets/dialogues/test.json"))
+        self.add_entity(Entity(self, "door3", (14, 52), 'D',
+                               [Event(data, self,"door3", "ON_INTERACT", "MOVE",
+                  target_scene=Test2, target_position=(5, 5))]))
+        
+        #transition to village1
+
+        self.add_entity(Entity(self, "teleporter1", (1, 36), ' ',
+                               [Event(data, self,"teleporter1", "ON_STEP", "MOVE",
+                  target_scene=Test, target_position=(3, 3))]))
+        
+        self.add_entity(Entity(self, "teleporter1", (1, 37), ' ',
+                               [Event(data, self,"teleporter1", "ON_STEP", "MOVE",
+                  target_scene=Test, target_position=(3, 3))]))
+        
+        self.add_entity(Entity(self, "teleporter1", (1, 38), ' ',
+                               [Event(data, self,"teleporter1", "ON_STEP", "MOVE",
+                  target_scene=Test, target_position=(3, 3))]))
+        
+        # transition to zoo map
+
+        self.add_entity(Entity(self, "teleporter1", (11, 1), ' ',
+                               [Event(data, self,"teleporter1", "ON_STEP", "MOVE",
+                  target_scene=Test, target_position=(3, 3))]))
+        
+        # npcs
+
+        self.add_entity(NPC(self, "npc1", (10, 32), 'N', dialogue="assets/dialogues/test.json"))
 
 
 class Test2(World):
