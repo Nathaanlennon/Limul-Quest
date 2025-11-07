c2b83e7fc74770a9f293ea057d86cc1caf6bd64d
Drackfish <tommonteiroalves@gmail.com>
2025-11-07 14:27:47 +0100

    bug

--- world.py
+++ world.py
@@ -3,13 +3,13 @@ from engine.core.base import World, Entity, Event, NPC
 
 class Test(World):
     def __init__(self, data, **kwargs):
-        super().__init__(data, "assets/maps/maptest.txt", (2, 3))
+        super().__init__(data, "assets/maps/maptest.txt", (3, 3))
         self.name = "Monde1"
 
-        self.add_entity(Entity(self, "door1", (4, 11), 'DD',
+        self.add_entity(Entity(self, "door1", (5, 5), 'D',
                                [Event(data, self,"door1", "ON_INTERACT", "MOVE",
-                  target_scene=Test2, target_position=(5, 5))]))
-        self.add_entity(Entity(self, "teleporter1", (5, 1), 'T',
+                  target_scene=Test2, target_position=(15, 5))]))
+        self.add_entity(Entity(self, "teleporter1", (3, 1), 'T',
                                [Event(data, self,"teleporter1", "ON_STEP", "MOVE",
                   target_scene=Test, target_position=(3, 3))]))
 
@@ -18,12 +18,15 @@ class Test(World):
 
 class Test2(World):
     def __init__(self, data, **kwargs):
-        super().__init__(data, "assets/maps/testchateau.txt", (5, 5))
+        super().__init__(data, "assets/maps/mapVillageV1.txt", (5, 5))
         self.name = "Monde2"
 
-        entity = self.add_entity(Entity(self,"door", (8, 24), 'D'))
-        entity.add_event(Event(data, self, "door","ON_INTERACT", "MOVE",
-                               target_scene=Test3))
+        self.add_entity(Entity(self,"door2", (8, 24), 'D',
+                               [Event(data, self,"door2", "ON_INTERACT", "MOVE",
+                                      target_scene=Test, target_position=(9, 35))]))
+        self.add_entity(Entity(self, "teleporter2", (11, 35), 'T',
+                               [Event(data, self, "teleporter2", "ON_STEP", "MOVE",
+                                      target_scene=Test2, target_position=(5, 5))]))
 
 
 class Test3(World):
